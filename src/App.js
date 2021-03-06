119f616432ea10c9b20b27a76eb7258ad2659ea1
Mathias <mathias@studierenplus.de>
2021-03-06 21:41:15 +0100

    added all previews +  heart beat

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import classes from './App.module.css';
 import ProductData from "./ProductData";
 
 function App() {
+  const currentHour = new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours();
+  const currentMinute = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
   return (
     <div className="App">
       <header className="App-header">
@@ -13,9 +15,35 @@ function App() {
         <div className={classes.MainContainer}>
             <div className={classes.ProductPreview}>
                 <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview"/>
+
+                {/*<div className={classes.TimeSection}>
+                    <p>{`${currentHour}:${currentMinute}`}</p>
+                </div>*/}
+
+                <div className={classes.HeartBeatSection}>
+                    <i class="fas fa-heartbeat"></i>
+                    <p>78</p>
+                </div>
             </div>
             <div className={classes.ProductData}>
-                <h1>{ProductData.title}</h1>
+                <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
+                <p className={classes.ProductDescription}>{ProductData.description}</p>
+
+                <h3 className={classes.SectionHeading}>Select Color</h3>
+                <div>
+                    <img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src="https://imgur.com/iOeUBV7.png" alt="Black Colored Watch" />
+                    <img className={classes.ProductImage} src="https://imgur.com/PTgQlim.png" alt="Red Colored Watch" />
+                    <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="Blue Colored Watch" />
+                    <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="Purple Colored Watch" />
+                </div>
+
+                <h3 className={classes.SectionHeading}>Features</h3>
+                <div>
+                    <button className={[classes.FeatureItem, classes.SelectedFeatureItem].join(' ')}>Time</button>
+                    <button className={classes.FeatureItem}>Heart Rate</button>
+                </div>
+
+                <button className={classes.PrimaryButton}>Buy Now</button>
             </div>
         </div>
     </div>
