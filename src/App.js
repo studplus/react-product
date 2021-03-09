6ef8e94cecbf9a509285c3d48aaf33d7354c2926
Mathias <mathias@studierenplus.de>
2021-03-09 15:56:01 +0100

    code into components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './App.module.css';
 import ProductPreview from "./ProductPreview";
 import ProductDetails from "./ProductDetails";
 import Topbar from "./Topbar";
+import ProductData from "./ProductData";
 
-function App() {
-  return (
-    <div className="App">
-        <Topbar />
+class App extends Component {
+    state = {
+        productData: ProductData,
+        currentPreviewImage: 'https://imgur.com/iOeUBV7.png',
+        showHeartBeatSection: true,
+    }
+  render() {
+      return (
+          <div className="App">
+              <Topbar />
 
-        <div className={classes.MainContainer}>
-            <div className={classes.ProductPreview}>
-                <ProductPreview />
-            </div>
-            <div className={classes.ProductData}>
-                <ProductDetails />
-            </div>
-        </div>
-    </div>
-  );
+              <div className={classes.MainContainer}>
+                  <div className={classes.ProductPreview}>
+                      <ProductPreview currentPreviewImage={this.state.currentPreviewImage}
+                                      showHeartBeatSection={this.state.showHeartBeatSection}/>
+                  </div>
+                  <div className={classes.ProductData}>
+                      <ProductDetails data={this.state.productData}/>
+                  </div>
+              </div>
+          </div>
+      );
+  }
 }
 
 export default App;
--- src/ProductPreview.js
+++ src/ProductPreview.js
@@ -6,16 +6,18 @@ const ProductPreview = (props) => {
     const currentMinute = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
     return(
         <div className={classes.ProductPreview}>
-            <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview"/>
-
-            <div className={classes.TimeSection}>
-                    <p>{`${currentHour}:${currentMinute}`}</p>
-                </div>
-
-            {/*<div className={classes.HeartBeatSection}>
-                <i className="fas fa-heartbeat"></i>
-                <p>78</p>
-            </div>*/}
+            <img src={props.currentPreviewImage} alt="Product Preview"/>
+            {
+                props.showHeartBeatSection ?
+                    <div className={classes.HeartBeatSection}>
+                        <i className="fas fa-heartbeat"></i>
+                        <p>78</p>
+                    </div>
+                    :
+                    <div className={classes.TimeSection}>
+                        <p>{`${currentHour}:${currentMinute}`}</p>
+                    </div>
+            }
         </div>
     );
 }
